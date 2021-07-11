9cf0df2a9b029c6cd44852db8d9f84902b01aea8
Sai chand <saichand@octalogic.in>
2021-07-11 17:39:23 +0530

    use api hook added

---
+++ Provider.js
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// export const ApiContext = React.createContext();
+
+// const APIProvider = ({ defaultWrapperFunction, children }) => {
+//   return (
+//     <ApiContext.Provider value={{ defaultWrapperFunction }}>
+//       {children}
+//     </ApiContext.Provider>
+//   );
+// };
+
+// export default APIProvider;
---
+++ index.js
@@ -0,0 +1,3 @@
+import useApi from "./use-api";
+
+export { useApi };
